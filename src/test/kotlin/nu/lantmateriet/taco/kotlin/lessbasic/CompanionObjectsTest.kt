ac2eef34f0d0a18e5c18a6c1e8a39e267f317405
Peter Vestlin <peter.vestlin@lm.se>
2019-11-07 09:50:16 +0100

    Lade till test för companion object extension fucntion.

--- src/test/kotlin/nu/lantmateriet/taco/kotlin/lessbasic/CompanionObjectsTest.kt
+++ src/test/kotlin/nu/lantmateriet/taco/kotlin/lessbasic/CompanionObjectsTest.kt
@@ -11,7 +11,7 @@ Jämför med static i Java.
 
 internal class CompanionObjectsTest {
 
-    private data class Person(val first: String, val last: String) {
+    data class Person(val first: String, val last: String) {
         companion object Factory {
             fun create(fullName: String): Person {
                 with(fullName.split(" ")) {
@@ -22,16 +22,15 @@ internal class CompanionObjectsTest {
     }
 
     @Test
+    @Suppress("RedundantCompanionReference")
     fun `skapa person med factory`() {
-        assertThat(Person.create("Mimmi Pigg")).isEqualTo(Person("Mimmi", "Pigg"))
+        assertThat(Person.Factory.create("Mimmi Pigg")).isEqualTo(Person("Mimmi", "Pigg"))
     }
 
     @Test
     fun `skapa person med factory fast utan att ange factory`() {
         assertThat(Person.create("Mimmi Pigg")).isEqualTo(Person("Mimmi", "Pigg"))
     }
-
-}
 
 /*
 Man kan även lägga till companion objects "utanför klassen, typ som extension functions.
@@ -41,3 +40,14 @@ data class Person(val first: String, val last: String) {
 }
 
  */
+
+    fun Person.Factory.createDonaldDuck() = Person("Donald", "Duck")
+
+    @Test
+    fun `Donald Duck`() {
+        with(Person.createDonaldDuck()) {
+            assertThat(first).isEqualTo("Donald")
+            assertThat(last).isEqualTo("Duck")
+        }
+    }
+}
